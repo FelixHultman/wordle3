37970db46ec6a65afa1a5b5a89e6b6825f9e3c42
Felix Hultman <karlfelixhultman@gmail.com>
2024-04-16 19:24:48 +0200

    done

--- frontend/src/components/guessMenu.jsx
+++ frontend/src/components/guessMenu.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 function GuessMenu({ guessWord, setGuessWord, handleGuess, guesses }) {
   return (
-    <div class='flex flex-col items-center text '>
-      <label htmlFor=''>
-        Write guess:
+    <div class='flex flex-col items-center text'>
+      <label htmlFor='' class='mt-4 mr-2 px-4 py-2 rounded p-1 ml-2 text-lg'>
+        <span class='text-3xl'>Write your guess:</span>
         <input
+          class='rounded mr-2 ml-2 mt-4  px-4 py-2'
           type='text'
           value={guessWord}
           onChange={(e) => setGuessWord(e.target.value.toUpperCase())}
         />
-        <button onClick={handleGuess}>Confirm guess</button>
+        <button
+          onClick={handleGuess}
+          class='mt-4 mr-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+        >
+          Confirm guess
+        </button>
       </label>
       <ul>
         {guesses
